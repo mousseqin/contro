ee12a6aa2f5b46596bbecd9e0e8d132c4fbb84ad
[mousseqin] <[qinchao_mousse@icloud.com]>
2021-03-03 14:24:29 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//use App\Http\Controllers\UserController;
+//Route::get('/user', [UserController::class, 'index']);
+
+//Route::get($uri, $callback);
+//Route::post($uri, $callback);
+//Route::put($uri, $callback);
+//Route::patch($uri, $callback);
+//Route::delete($uri, $callback);
+//Route::options($uri, $callback);
+
+//Route::match(['get', 'post'], '/', function () {
+//    //
+//});
+//
+//Route::any('/', function () {
+//    //
+//});
+
+//Route::redirect('/here', '/there');
+//Route::redirect('/here', '/there', 301);
+//Route::permanentRedirect('/here', '/there');
+
+//Route::view('/welcome', 'welcome');
+//Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+//Route::get('user/{id}', function ($id) {
+//    return 'User '.$id;
+//});
+//Route::get('posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//    //
+//});
